006d630bb798018a302bf305c6309c13e5e85389
mariaxarisi <157244698+mariaxarisi@users.noreply.github.com>
2024-10-29 23:23:05 +0200

    Create randomMatrix.c

---
+++ src/randomMatrix.c
@@ -0,0 +1,26 @@
+#include <../include/knn.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void createMatrix(Matrix* matrix, size_t points, size_t dimensions){
+    matrix->rows = points;
+    matrix->cols = dimensions;
+    matrix->data = (double*)malloc(points*dimensions*sizeof(double));
+    if(matrix->data == NULL){
+        printf("Problem with memory allocation in createMatrix");
+    }
+}
+
+void randomMatrix(Matrix* matrix, size_t points, size_t dimensions){
+    matrix->rows = points;
+    matrix->cols = dimensions;
+    matrix->data = (double*)malloc(points*dimensions*sizeof(double));
+    if(matrix->data == NULL){
+        printf("Problem with memory allocation in randomMatrix");
+    }
+
+    for(size_t i=0; i<points*dimensions; i++){
+        matrix->data[i] = ((double)rand()/RAND_MAX)*100;
+    }
+}
