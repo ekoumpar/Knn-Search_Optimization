308d1332469e93dc8f35cd1d702a540e24bcdcc1
ekoumpar <cdkhelen@gmail.com>
2024-11-09 13:03:53 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -11,59 +11,13 @@ int main(){
     
     Matrix C, Q, D, K;
     size_t k;
-    const char* filename = "data.mat";
+    const char* filename="data.mat";
     mat_t *file = NULL;
 
-    
-    //open file 
-    file=Mat_Open(filename, MAT_ACC_RDONLY);
-    if (file == NULL) {
-        printf("Error in opening given Matfile");
-    }
+    OpenFile(&file, filename);
 
-    //get Corpus
-    matvar_t* arrayPr = Mat_VarRead(file, "CORPUS");
-    if (arrayPr == NULL && arrayPr->data == NULL) {
-        printf("Array pointer or data pointer in array is NULL.\n");
-        Mat_VarFree(arrayPr);
-    }
-
-    double* dataPr = (double*)arrayPr->data;
-    
-    C.rows = (size_t)arrayPr->dims[0];
-    C.cols = (size_t)arrayPr->dims[1];
-    
-    C.data = (double*)malloc(C.rows*C.cols * sizeof(double));
-    if (C.data == NULL) {
-        printf("Memory allocation for array->data failed.\n");
-        Mat_VarFree(arrayPr);
-    }
-    for (int i = 0; i < C.rows * C.cols; i++) {
-        C.data[i] = dataPr[i];
-    }
-    Mat_VarFree(arrayPr);
-
-    //get Query
-    arrayPr = Mat_VarRead(file, "QUERY");
-    if (arrayPr == NULL && arrayPr->data == NULL) {
-        printf("Array pointer or data pointer in array is NULL.\n");
-        Mat_VarFree(arrayPr);
-    }
-
-    dataPr = (double*)arrayPr->data;
-
-    Q.rows = (size_t)arrayPr->dims[0];
-    Q.cols = (size_t)arrayPr->dims[1];
-
-    Q.data = (double*)malloc(Q.rows * Q.cols * sizeof(double));
-    if (Q.data == NULL) {
-        printf("Memory allocation for array->data failed.\n");
-        Mat_VarFree(arrayPr);
-    }
-    for (int i = 0; i < Q.rows *Q.cols; i++) {
-        Q.data[i] = dataPr[i];
-    }
-    Mat_VarFree(arrayPr);
+    readMatrix(&C, "CORPUS", file);
+    readMatrix(&Q, "QUERY", file);
 
     
     printf("Enter the number of nearest neighbours: ");
@@ -80,16 +34,16 @@ int main(){
    
 
     printf("---------------Corpus-Set----------------");
-    //printMatrix(&C);
+    printMatrix(&C);
     
     
     printf("---------------Query-Set-----------------");
-    //printMatrix(&Q);
+    printMatrix(&Q);
     
     //Calculate Distances
     distanceBlas(&C, &Q, &D);
     printf("---------------Distances-----------------");
-    //printMatrix(&D);
+    printMatrix(&D);
         
     //Sorting of kth elements
     int rows = (int)D.rows;
@@ -110,7 +64,6 @@ int main(){
     free(Q.data);
     free(D.data);
     
-    Mat_Close(file);
+    CloseFile(&file);
     return 0;
 }
-
