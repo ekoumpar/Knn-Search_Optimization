cae35d6e90255a276001c3b7092e5467dc548cf0
ekoumpar <cdkhelen@gmail.com>
2024-11-10 20:08:32 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -11,35 +11,46 @@ int main(){
     
     Matrix C, Q, D, K;
     size_t k;
-    const char* filename="data.mat";
+    const char* filename="small.mat";
     mat_t *file = NULL;
 
     OpenFile(&file, filename);
-
+   
+    //get Corpus
     readMatrix(&C, "CORPUS", file);
-    readMatrix(&Q, "QUERY", file);
+
+    //for debugging
+    printf("---------------Corpus-Set----------------");
+    printMatrix(&C);
+    
+    double* data_C = C.data;
+    VPNode* Corpus = NULL;
+
+    buildVPTree(&C, &Corpus);
+
+    //for debugging
+    printf("\n\n");
+    printVPTree(Corpus, 0, C.cols);
+    printf("\n\n");
+
+    free(data_C);//delete C
 
     
+    readMatrix(&Q, "QUERY", file);
     printf("Enter the number of nearest neighbours: ");
      if (scanf("%zu", &k) != 1) {
           printf("Invalid input for 'k'.\n");
           return -1;
         }
-    
-   
+    printf("---------------Query-Set-----------------");
+    printMatrix(&Q);
+
+    /*
     size_t corpus = C.rows;
     size_t query = Q.rows;
     createArray(&D, query, corpus);
     createArray(&K, query, k);
    
-
-    printf("---------------Corpus-Set----------------");
-    printMatrix(&C);
-    
-    
-    printf("---------------Query-Set-----------------");
-    printMatrix(&Q);
-    
     //Calculate Distances
     distanceBlas(&C, &Q, &D);
     printf("---------------Distances-----------------");
@@ -55,15 +66,13 @@ int main(){
 
     printf("---------------K neighbours----------------");
     printMatrix(&K);
+    */
+    //free(K.data);
+    //free(D.data);
     
- 
 
-  
-    free(K.data);
-    free(C.data);
     free(Q.data);
-    free(D.data);
-    
+    freeVPTree(&Corpus);
     CloseFile(&file);
     return 0;
 }
