1f02d2fdaa34f60a7a269d9c43981f196d40eeee
ekoumpar <cdkhelen@gmail.com>
2024-11-10 20:11:05 +0200

    Update knn.h

--- include/knn.h
+++ include/knn.h
@@ -6,22 +6,37 @@
 
 typedef struct {
     double* data;
-    size_t rows;
     size_t cols;
+    size_t rows;
 }Matrix;
 
-void createMatrix(Matrix* matrix, size_t points, size_t dimensions);
+typedef struct VPNode {
+    double* point;  //Vantage point      
+    double radius;  //median        
+    struct VPNode* left;
+    struct VPNode* right;
+}VPNode;
+
+
+
+void createArray(Matrix* matrix, size_t points, size_t dimensions);
 void randomMatrix(Matrix* matrix, size_t points, size_t dimensions);
 void printMatrix(const Matrix* matrix);
 void OpenFile(mat_t** file, const char* filename);
 void readMatrix(Matrix* matrix, const char* matrixName, mat_t* file);
 void CloseFile(mat_t** file);
 
+void buildVPTree(Matrix* matrix, VPNode** node);
+void freeVPTree(struct VPNode** node);
+void printVPTree(struct VPNode* node, int level, int dim);
+
+
 void quickSelect(double* arr, int low, int high, int k, double* kneighbour);
 void swap(double* a, double* b);
 void quickSort(double* arr, int low, int high);
 int partition2(double* arr, int low, int high);
 int partition(double* arr, int low, int high);
+double quickMedian(double* arr, int low, int high);
 
 void distanceBlas(const Matrix* C, const Matrix* Q, Matrix* D);
 
