a4017d6d5ed7474fb775f52cdb3056f0beb24137
ekoumpar <cdkhelen@gmail.com>
2024-11-18 14:07:03 +0200

    Indexing functions_cpp.h

--- include/functions_cpp.h
+++ include/functions_cpp.h
@@ -1,10 +1,18 @@
-#ifndef FUNCTIONS_CPP_H
-#define FUNCTIONS_CPP_H
+#include <queue>
+#include <ostream>
+#include <iostream>
 
-#include <stddef.h>
-#include <vector>
-#include "vptree.h"
+using namespace std;
+struct Neighbor {
+    double distance;
+    int index;
+};
+struct Compare {
+    bool operator()(Neighbor const& p1, Neighbor const& p2) const {
+        return p1.distance < p2.distance; 
+    }
+};
+void searchVPTree(VPNode* node, double* query, int dim,int k, priority_queue<Neighbor,vector<Neighbor>,Compare>& pq, double threshold);
 
-void searchVPTree(VPNode* node, double* query, int dim, std::vector<double>& distances, double threshold);
 
 #endif 
