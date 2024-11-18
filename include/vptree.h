d6ccf1bf98be526bfd5066b8d5ee719836296b39
ekoumpar <cdkhelen@gmail.com>
2024-11-18 14:11:31 +0200

    Indexing vptree.h

--- include/vptree.h
+++ include/vptree.h
@@ -2,10 +2,12 @@
 #define VPTREE_H
 
 typedef struct VPNode {
+    int index;
     double* point;  // Vantage point      
     double radius;  // Median        
     struct VPNode* left;
     struct VPNode* right;
 } VPNode;
+
 
 #endif // VPTREE_H
