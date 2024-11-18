2ff79c34fbaf03def6a7bb978acac7500be96cc6
ekoumpar <cdkhelen@gmail.com>
2024-11-18 14:05:02 +0200

    Indexing main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,14 @@
 #include <omp.h>
 #include "matio.h"
 #include <vector>
+#include <iostream>
+#include <queue>
+#include <cstdio>
 
 int main(){
     srand(time(NULL));
     
-    Matrix C, Q, K;
+    Matrix C,Q,K,Kindex;
     size_t k;
     mat_t* wFile = NULL;
     
@@ -50,29 +53,48 @@ int main(){
 
     //Start timer
     double start_time = omp_get_wtime();
+   
+    int* kindex = (int*)malloc(sizeof(int) * C.rows);
+    for (int i = 0; i < C.rows; i++) {
+        kindex[i] = i + 1;
+    }
 
-    //after bulidVPTree C is deleted!
     VPNode* Corpus = NULL;
-    buildVPTree(&C, &Corpus, &totalDistance, &totalNodes);
+    buildVPTree(&C, &Corpus, &totalDistance, &totalNodes,&kindex);
 
     //Search for k nearest neighbours
-    createMatrix(&K, Q.rows, k);
     double threshold = (totalDistance / totalNodes)*0.1 ;
-   
+    std::vector<std::vector<Neighbor>> allNeighbors(Q.rows); // structure with k nearest neighbours and indexes
 
     #pragma omp parallel for
-    for(size_t i = 0; i < Q.rows; i++){
-        std::vector<double> distances;
-        searchVPTree(Corpus, Q.data + i*Q.cols, (int)Q.cols, distances, threshold);
-        quickSelect(distances.data(), 0, distances.size()-1, k, K.data + i*k);
-    }
+        for (size_t i = 0; i < Q.rows; i++){
+            using namespace std;
+            priority_queue<Neighbor, vector<Neighbor>, Compare> pq;
+            searchVPTree(Corpus, Q.data + i * Q.cols, (int)Q.cols, (int)k, pq, threshold);
+            std::vector<Neighbor> neighbors;
+            while (!pq.empty()) {
+            neighbors.push_back(pq.top());
+            pq.pop();
+            }
+            allNeighbors[i] = std::move(neighbors);
+        }
 
     //End timer
     double end_time = omp_get_wtime();
-
-    //Print k nearest neighbours
-    printf("---------------%zu-Nearest-Neighbours-----------------", k);
-    printMatrix(&K);
+    
+    createMatrix(&K, Q.rows, k);
+    createMatrix(&Kindex, Q.rows, k);
+    //printing output
+    for (size_t i = 0; i < allNeighbors.size(); i++) {
+        std::cout << "Query " << i << ": " << std::endl;
+        size_t j = 0;
+        for (const auto& neighbor : allNeighbors[i]) {
+            std::cout << "  Index: " << neighbor.index << ", Distance: " << neighbor.distance << std::endl;
+            K.data[i * K.cols + j] = neighbor.distance;
+            Kindex.data[i * K.cols + j] = static_cast<double>(neighbor.index);
+            j++;
+        }
+    }
 
     printf("Threshold: %f\n", threshold);
     printf("Time taken for calculate k neighbours: %f seconds\n", end_time - start_time);
@@ -80,6 +102,7 @@ int main(){
     //save results in output.mat
     createFile(&wFile);
     saveMatrix(&wFile, K.rows, K.cols, K.data, "K");
+    saveMatrix(&wFile, Kindex.rows, Kindex.cols, Kindex.data, "Kindex");
     CloseFile(&wFile);
 
     free(Q.data);
