3ee16b4a4e0dee76cede7eb911e30927ddac0fa7
ekoumpar <cdkhelen@gmail.com>
2024-11-18 14:35:28 +0200

    Indexing readMatFiles.c

--- src/readMatFiles.c
+++ src/readMatFiles.c
@@ -9,12 +9,8 @@ void OpenFile(mat_t **file, const char* filename){
     if (*file == NULL) {
         printf("Opening file failed\n");
         return;
-       
     }
-    
-
     printf("File opened succesfully\n");
-
 }
 
 void readMatrix(Matrix *matrix,const char* matrixName, mat_t *file) {
@@ -51,10 +47,37 @@ void CloseFile(mat_t** file) {
     }
 }
 
-void createFile(mat_t** file) {
+void CreateFile(mat_t** file) {
 
     *file = Mat_CreateVer("output.mat", NULL, MAT_FT_MAT5);
     if (*file == NULL) {
         fprintf(stderr, "Σφάλμα: Αδυναμία δημιουργίας MAT αρχείου.\n");
     }
+}
+
+
+void saveMatrix(mat_t** file, int rows, int cols, double* data, const char* name) {
+
+    size_t dims[2] = { (size_t)rows,(size_t)cols };
+    double* dataPr = (double*)malloc(rows * cols * sizeof(double));
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            dataPr[j * rows + i] = data[i * cols + j];
+        }
+    }
+    matvar_t* matvar = Mat_VarCreate(name, MAT_C_DOUBLE, MAT_T_DOUBLE, 2, dims, dataPr, 0);
+    if (matvar == NULL) {
+        fprintf(stderr, "Σφάλμα: Αδυναμία δημιουργίας MAT μεταβλητής.\n");
+        Mat_Close(*file);
+        return;
+    }
+
+    if (Mat_VarWrite(*file, matvar, MAT_COMPRESSION_NONE) != 0) {
+        fprintf(stderr, "Σφάλμα: Αδυναμία εγγραφής της μεταβλητής στο MAT αρχείο.\n");
+        return;
+    }
+    free(dataPr);
+    Mat_VarFree(matvar);
+
 }
